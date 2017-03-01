ed0821b52848dee3a35c7a30338408d969189178
yanxuewen <yanxuewen325@163.com>
2017-03-01 21:11:39 +0800

    init

---
+++ YAVSpeechSynthesizerDemo/YAVSpeechSynthesizerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YAVSpeechSynthesizerDemo
+//
+//  Created by yanxuewen on 2017/3/1.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YAVSpeechSynthesizerDemo/YAVSpeechSynthesizerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  YAVSpeechSynthesizerDemo
+//
+//  Created by yanxuewen on 2017/3/1.
+//  Copyright © 2017年 YXW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
